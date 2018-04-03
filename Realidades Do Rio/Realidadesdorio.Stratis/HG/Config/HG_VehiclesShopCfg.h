ffef8c2474b927073fbb63e79fa08dcf0380b167
Marcus Vinícius <marvinn2002@hotmail.com>
2018-04-02 21:20:21 -0300

    Valores dos veículos reajustados

--- Realidades Do Rio/Realidadesdorio.Stratis/HG/Config/HG_VehiclesShopCfg.h
+++ Realidades Do Rio/Realidadesdorio.Stratis/HG/Config/HG_VehiclesShopCfg.h
@@ -33,17 +33,17 @@ class HG_DefaultShop // HG_DefaultShop is just a placeholder for testing purpose
 	    displayName = "Veiculos Civis";
 		vehicles[] =
 		{
-		    {"C_SUV_01_F",2000,"true"},
-			{"walker_a3_gtigolf",2000,"true"},
-			{"C_Offroad_01_F",2400,"true"},
-			{"C_Hatchback_01_sport_F",2000,"true"},
+		    {"C_SUV_01_F",12000,"true"},
+			{"walker_a3_gtigolf",9000,"true"},
+			{"C_Offroad_01_F",11000,"true"},
+			{"C_Hatchback_01_sport_F",12000,"true"},
 			{"C_Quadbike_01_F",1000,"true"},
-			{"C_Offroad_02_unarmed_F_black",2450,"true"},
-			{"C_Van_02_vehicle_F",3250,"true"},
-			{"c4hch_civ",1900,"true"},
-			{"206_vl",1800,"true"},
-			{"chfcrsciv",2100,"true"},
-			{"AC1D_ford_mustang_NFS_SPEC",4000,"true"}
+			{"C_Offroad_02_unarmed_F_black",11000,"true"},
+			{"C_Van_02_vehicle_F",9900,"true"},
+			{"c4hch_civ",16000,"true"},
+			{"206_vl",19000,"true"},
+			{"chfcrsciv",18000,"true"},
+			{"AC1D_ford_mustang_NFS_SPEC",32000,"true"}
 		};
 	    spawnPoints[] =
 		{
@@ -62,17 +62,17 @@ class HG_DefaultShop2 // HG_DefaultShop is just a placeholder for testing purpos
 	    displayName = "Veiculos Civis";
 		vehicles[] =
 		{
-		    {"C_SUV_01_F",2000,"true"},
-			{"walker_a3_gtigolf",2000,"true"},
-			{"C_Offroad_01_F",2400,"true"},
-			{"C_Hatchback_01_sport_F",2000,"true"},
+		    {"C_SUV_01_F",12000,"true"},
+			{"walker_a3_gtigolf",9000,"true"},
+			{"C_Offroad_01_F",11000,"true"},
+			{"C_Hatchback_01_sport_F",12000,"true"},
 			{"C_Quadbike_01_F",1000,"true"},
-			{"C_Offroad_02_unarmed_F_black",2450,"true"},
-			{"C_Van_02_vehicle_F",3250,"true"},
-			{"c4hch_civ",1900,"true"},
-			{"206_vl",1800,"true"},
-			{"chfcrsciv",2100,"true"},
-			{"AC1D_ford_mustang_NFS_SPEC",4000,"true"}
+			{"C_Offroad_02_unarmed_F_black",11000,"true"},
+			{"C_Van_02_vehicle_F",9900,"true"},
+			{"c4hch_civ",16000,"true"},
+			{"206_vl",19000,"true"},
+			{"chfcrsciv",18000,"true"},
+			{"AC1D_ford_mustang_NFS_SPEC",32000,"true"}
 		};
 	    spawnPoints[] =
 		{
