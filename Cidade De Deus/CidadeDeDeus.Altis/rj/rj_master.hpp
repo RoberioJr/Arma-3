bfcb85c8ac63a9ee30f96c3001e67bbdc54e83b8
RoberioJr <rrpontesjunior10@gmail.com>
2018-06-06 21:18:57 -0300

    FIX

--- Cidade De Deus/CidadeDeDeus.Altis/rj/rj_master.hpp
+++ Cidade De Deus/CidadeDeDeus.Altis/rj/rj_master.hpp
@@ -9,5 +9,4 @@
 #include "MenuAdminRJ\VeiculoSpawn\TUT_gui_veh.hpp"                    //Menu De Veiculos ADM
 #include "MenuAdminRJ\VAS\menu.hpp"                                    //Menu De Equipamento ADM
 #include "derleon\Scripts_Master.hpp"                                  //Auto Save Do Jogador
-#include "Variados\Colocaveis\Dialogs\Defines.hpp"                  //Menu De Objetos
-#include "Variados\Colocaveis\Dialogs\GUI.hpp"                      //Menu De Objetos
+#include "Variados\Colocaveis\Dialogs\GUI.hpp"                         //Menu De Objetos
