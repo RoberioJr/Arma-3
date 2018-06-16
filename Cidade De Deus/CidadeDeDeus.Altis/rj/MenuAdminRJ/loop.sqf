39a07b037f197974f885517c09bba2825d586d55
RoberioJr <rrpontesjunior10@gmail.com>
2018-06-16 18:32:34 -0300

    Finalização Do AltisLife

--- Cidade De Deus/CidadeDeDeus.Altis/rj/MenuAdminRJ/loop.sqf
+++
@@ -1,7 +0,0 @@
-while {true} do
-{
-  waitUntil {alive vehicle player};
- [] execVM "rj\MenuAdminRJ\admins.sqf";
-
-  waitUntil {!alive player};
-};
--- Cidade De Deus/CidadeDeDeus.Altis/rj/rj_CfgFunctions.hpp
+++
@@ -1,9 +0,0 @@
-/*
-
-    Author: RobérioJR Asfarc [ASF]
-
-*/
-
-
-#include "MenuAdminRJ\VAS\cfgfunctions.hpp"     //Menu De Equipamento ADM
-#include "Logo\rj_logofunc.h"                   //Marca D'Agua Com Logo
